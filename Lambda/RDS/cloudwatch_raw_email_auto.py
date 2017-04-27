0946e92b8af152ae1cffe80377eda18a88d6b12d
dowun55 <dowun55@gmail.com>
2017-04-27 11:56:15 +0900

    asdfasf

--- Lambda/RDS/cloudwatch_raw_email_auto.py
+++ Lambda/RDS/cloudwatch_raw_email_auto.py
@@ -3,37 +3,46 @@ import datetime
 from datetime import date
 from datetime import timedelta
 
-client = boto3.client('cloudwatch')
-dbName = 'seoul-csy-test'
-startDay = date.today() - timedelta(1)
-startTime = datetime.time(00, 00, 00)
-startDate = datetime.datetime.combine(startDay, startTime)
+def lambda_handler(event, context):
 
-endDay = date.today()
-endTime = datetime.time(00, 00, 00)
-endDate = datetime.datetime.combine(endDay, endTime)
+    sns = boto3.client('sns')
+    client = boto3.client('cloudwatch')
 
-response = client.get_metric_statistics(
-    Namespace = 'AWS/RDS',
-    MetricName = 'CPUUtilization',
-    Dimensions = [
-        {
-            'Name': 'DBInstanceIdentifier',
-            'Value': dbName
-        },
-    ],
-    StartTime = startDate,
-    EndTime = endDate,
-    Period = 3600,
-    Statistics = [ 'Maximum' ],
-)
+    dbName = 'seoul-csy-test'
 
-result = []
-for printData in response['Datapoints']:
-    result.append(print (str(printData['Timestamp']), printData['Maximum']))
+    startDay = date.today() - timedelta(1)
+    startTime = datetime.time(00, 00, 00)
+    startDate = datetime.datetime.combine(startDay, startTime)
 
-response = sns.publish(
-            TopicArn = 'arn:aws:sns:ap-northeast-2:449635015751:CSY-SNS-TEST',
-            Subject = '[AWS알림] RDS 일일 리포트',
-            Message = result
-        )
+    endDay = date.today()
+    endTime = datetime.time(00, 00, 00)
+    endDate = datetime.datetime.combine(endDay, endTime)
+
+    response = client.get_metric_statistics(
+        Namespace = 'AWS/RDS',
+        MetricName = 'CPUUtilization',
+        Dimensions = [
+            {
+                'Name': 'DBInstanceIdentifier',
+                'Value': dbName
+            },
+        ],
+        StartTime = startDate,
+        EndTime = endDate,
+        Period = 3600,
+        Statistics = [ 'Maximum' ],
+    )
+
+    result = []
+    for printData in response['Datapoints']:
+        result.append([str(printData['Timestamp']),printData['Maximum']])
+
+    result.sort()
+
+    message_body= "\n".join(map(str,result))
+
+    response = sns.publish(
+                TopicArn = 'arn:aws:sns:ap-northeast-2:449635015751:CSY-SNS-TEST',
+                Subject = '[AWS알림] RDS seoul-csy-test 일일 리포트',
+                Message = message_body
+            )
